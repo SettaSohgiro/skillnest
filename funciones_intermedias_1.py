4a2bf4f5c6322bd950e46866f66234456fd4256f
Lientur <telefonica.prueba2021@gmail.com>
2026-04-18 00:03:49 -0400

    trabajo bucle basico 1

--- funciones_intermedias_1.py
+++ funciones_intermedias_1.py
@@ -15,20 +15,16 @@ coordenadas = [
     {"latitud": 8.2588997, "longitud": -84.9399704}
 ]
 
-# Cambia el valor de 3 en matriz por 6. Una vez realizado el cambio tu matriz debería ser: [ [10, 15, 20], [6, 7, 14] ]
 matriz[1][0] = 6
 
 print(matriz)
 
-# Cambia el nombre del primer cantante de “Ricky Martin” a “Enrique Martin Morales”
 cantantes[0]["nombre"] = "Enrique Martin Morales"
 print(cantantes)
 
-# En ciudades, cambia “Cancún” por “Monterrey”
 ciudades["México"][2] = "Monterrey"
 print(ciudades)
 
-# En las coordenadas, cambia el valor de “latitud” por 9.9355431
 coordenadas[0]["latitud"] = 9.9355431
 print(coordenadas)
 
